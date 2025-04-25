57f897dcc9f7429d6db25425796c8e1075d6be1a
Tpotsk <tpotsk@gmail.com>
2025-04-24 23:32:34 -0300

    Add files via upload

--- projeto2.c
+++ projeto2.c
@@ -3,24 +3,24 @@
 #include <time.h>
 #include <locale.h>
 
-#define tam 5
+#define tam 10
 
 void copymatrix(int msource[tam][tam], int mdest[tam][tam]){
-    for(int j =0; j < tam; j++){
-        for (int i=0; i<tam; i++){
-            mdest[j][i] = msource[j][i];
+    for(int i =0; i < tam; i++){
+        for (int j=0; j<tam; j++){
+            mdest[i][j] = msource[i][j];
         }
     }
 }
 
 
 void printmatrix(int matriz[tam][tam]){
-    for(int j =0; j < tam; j++){
-            for (int i=0; i<tam; i++){
-                if(matriz[j][i]== 0){
+    for(int i =0; i < tam; i++){
+            for (int j=0; j<tam; j++){
+                if(matriz[i][j]== 0){
                     printf("- ");
                 }
-                else{
+                if(matriz[i][j]== 1){
                     printf("V ");
                 }
             }
@@ -28,7 +28,40 @@ void printmatrix(int matriz[tam][tam]){
         }
 }
 
+int count(int matrizA[tam][tam], int matrizB[tam][tam]){
+    int vivo;
+    for(int i=0; i<tam;i++){
+            for(int j=0;j<tam;j++){
+                vivo =0;
 
+                for(int a=(i-1); a<(i+1);a++){
+                    for(int b=(j-1);b<(j+1);b++){
+                        if(a == b){
+                            continue;
+                        }
+                        if (a > 0 && a < tam && b > 0 && b < tam){
+                            vivo += matrizA[a][b];
+                        }
+                        //printf("%i", matrizA[i-a][j-b]);
+                    }
+                }
+
+                if(matrizA[i][j] == 1){
+                    if(vivo < 2 || vivo > 3){
+                        matrizB[i][j] = 0;
+                    }else{
+                        matrizB[i][j] = 1;
+                    }
+                }else{
+                    if(vivo == 3){
+                        matrizB[i][j] = 1;
+                    }else{
+                        matrizB[i][j] = 0;
+                    }
+                }
+            }
+        }
+}
 int main(){
 
     setlocale(LC_ALL, "portuguese");
@@ -36,63 +69,28 @@ int main(){
 
 
     //Define as duas matrizes, uma tabuleiro e outra de apoio.
-    int A[tam][tam], B[tam][tam],qtd;
+    int A[tam][tam], B[tam][tam];
 
     // Realiza a primeira geração
-    for(int j =0; j < tam; j++){
-        for (int i=0; i<tam; i++){
-            A[j][i]= rand() %2;
+    for(int i =0; i < tam; i++){
+        for (int j=0; j<tam; j++){
+            A[i][j]= rand() %2;
         }
     }
 
-    printf("\n1° Geração: \n");
+    printf("\nSeed: \n");
     printmatrix(A);
 
     // Copia matriz A para B
     copymatrix(A,B);
 
 
-    for(int k = 0; k < 2; k++){
-
+    for(int k = 0; k < 4; k++){
         // Copia Matriz B para A
         copymatrix(B,A);
 
-        printf("\n%d° Geração: \n", k+2);
-        for(int j=0; j<tam;j++){
-            for(int i=0;i<tam;i++){
-                qtd =0;
-                if(j == 0 || i == 0 || j == tam-1 || i == tam -1){
-                    if(j ==0 && i == 0){
-                        qtd = A[1][0] + A[0][1] + A[1][1];
-                    }
-                    if(j == tam-1 && i == 0){
-                        qtd = A[j][i+1] + A[j+1][i+1] + A[j][i+1];
-                    }
-                    if(j == tam-1 && i == tam -1){
-                        qtd = A[j-1][i] + A[j-1][i-1] + A[j][i-1];
-                    }
-                    if(j == tam-1 && i == tam -1){
-                        qtd = A[j-1][i] + A[j-1][i-1] + A[j][i-1];
-                    }
-
-                    if(qtd > 3 || qtd <2){
-                        B[j][i] = 0;
-                        }
-                    else{
-                        B[j][i] = 1;
-                    }
-
-                }
-                else{
-                    qtd = A[j-1][i-1] + A[j][i-1] + A[j+1][i-1] + A[j-1][i] + A[j+1][i] + A[j-1][i+1] + A[j][i+1] + A[j+1][i+1];
-                    if(qtd > 3 || qtd <2){
-                        B[j][i] = 0;
-                    } else{
-                        B[j][i] = 1;
-                    }
-                }
-            }
-        }
+        printf("\n%d° Geração: \n", k+1);
+        count(A,B);
 
         printmatrix(B);
     }
